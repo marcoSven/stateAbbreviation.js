17061088a46686f7885632073a3d99d1d7368e7c
Marco Pietschmann <me@marcosven.com>
2018-02-18 15:50:53 -0800

    add stateAbbreviation.js

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # stateAbbreviation.js
 Convert state abbreviation to name and vice versa.
+
+## Use
+
+```js
+
+var stateList = new stateAbbreviation();
+
+// get the list
+stateList.list;
+
+// optionally get the list upper or lower case
+stateList.caseTransform( 'upper' );
+
+// Convert - pass a state name or abbreviation 
+// and optionally the desired type of capitalization
+stateList.stateConvert( 'CA', 'lower' );
+
+```
---
+++ stateAbbreviation.js
@@ -0,0 +1,167 @@
+/*!
+  stateAbbreviation.js 1.0.0
+  license: GNU GENERAL PUBLIC LICENSE
+  https://github.com/marcoSven/stateAbbreviation.js
+  https://marcoSven.com/
+*/
+
+var stateAbbreviation = function() {
+
+  var self = this;
+
+  this.list = [
+      ['AL','Alabama'],
+      ['AK','Alaska'],
+      ['AZ','Arizona'],
+      ['AR','Arkansas'],
+      ['CA','California'],
+      ['CO','Colorado'],
+      ['CT','Connecticut'],
+      ['DE','Delaware'],
+      ['DC','Washington DC'],
+      ['FL','Florida'],
+      ['GA','Georgia'],
+      ['GU','Guam'],
+      ['HI','Hawaii'],
+      ['ID','Idaho'],
+      ['IL','Illinois'],
+      ['IN','Indiana'],
+      ['IA','Iowa'],
+      ['KS','Kansas'],
+      ['KY','Kentucky'],
+      ['LA','Louisiana'],
+      ['ME','Maine'],
+      ['MD','Maryland'],
+      ['MA','Massachusetts'],
+      ['MI','Michigan'],
+      ['MN','Minnesota'],
+      ['MS','Mississippi'],
+      ['MO','Missouri'],
+      ['MT','Montana'],
+      ['NE','Nebraska'],
+      ['NV','Nevada'],
+      ['NH','New Hampshire'],
+      ['NJ','New Jersey'],
+      ['NM','New Mexico'],
+      ['NY','New York'],
+      ['NC','North Carolina'],
+      ['ND','North Dakota'],
+      ['OH','Ohio'],
+      ['OK','Oklahoma'],
+      ['OR','Oregon'],
+      ['PA','Pennsylvania'],
+      ['PR','Puerto Rico'],
+      ['RI','Rhode Island'],
+      ['SC','South Carolina'],
+      ['SD','South Dakota'],
+      ['TN','Tennessee'],
+      ['TX','Texas'],
+      ['VI','US Virgin Islands'],
+      ['UT','Utah'],
+      ['VT','Vermont'],
+      ['VA','Virginia'],
+      ['WA','Washington'],
+      ['WV','West Virginia'],
+      ['WI','Wisconsin'],
+      ['WY','Wyoming']
+    ];
+
+  Array.prototype.toLowerCase = function() {
+
+    return this.map( function( t ) {
+
+        return t.toLowerCase();
+
+    });
+
+  };
+
+  Array.prototype.toUpperCase = function() {
+
+    return this.map( function( t ) {
+
+      if ( t.toUpperCase ) {
+
+        return t.toUpperCase();
+
+      }
+
+      return t;
+
+    });
+
+  };
+
+  this.caseTransform = function ( caseTo ) {
+
+    switch( caseTo ) {
+
+      case "lower":
+
+          return self.list.toLowerCase();
+
+      case "upper":
+
+          return self.list.toUpperCase();
+
+      default:
+
+          return self.list;
+
+    }
+
+  }
+
+  this.stateConvert = function ( state, caseTo ) {
+
+    var states = self.list,
+        state = state.toUpperCase(),
+        result,
+        i;
+
+    if ( state.length === 2 ) {
+
+      for ( i = states.length - 1; i >= 0; i--) {
+
+        if ( states[i][0] === state ) {
+
+          result = states[i][1];
+
+        }
+
+      }
+
+    } else {
+
+      for ( i = states.length - 1; i >= 0; i--) {
+
+        if ( states[i][1].toUpperCase() === state ) {
+
+          result = states[i][0];
+
+        }
+
+      }
+
+    }
+
+    switch( caseTo ) {
+
+      case "lower":
+
+          return result.toLowerCase();
+          break;
+
+      case "upper":
+
+          return result.toUpperCase();
+
+      default:
+
+          return result;
+
+    }
+
+  }
+
+};
